6bdf20d9dc9c285d1715249e594474582a651bf2
han.huang <han.huang@sioeye.com>
2020-06-01 09:29:15 +0000

    firstcommit

---
+++ cloud-basic/eureka-server/src/main/java/com/hyving/ocean/eureka/EurekaServerApp.java
@@ -0,0 +1,19 @@
+package com.hyving.ocean.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: Hyving 黄瀚
+ * @date: 2020/6/1
+ * han.huang@sioeye.com
+ * Chengdu Tianfu Software Park
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApp.class,args);
+    }
+}
---
+++ ocean-article/src/main/java/com/hyving/ocean/article/ArticleApp.java
@@ -0,0 +1,17 @@
+package com.hyving.ocean.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Hyving 黄瀚
+ * @date: 2020/6/1
+ * han.huang@sioeye.com
+ * Chengdu Tianfu Software Park
+ */
+@SpringBootApplication
+public class ArticleApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApp.class,args);
+    }
+}
